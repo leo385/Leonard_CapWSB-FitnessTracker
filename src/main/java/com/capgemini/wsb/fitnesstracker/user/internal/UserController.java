9ef2cb369b2cde7c65e577b78540f7a09d606ff7
leonard <leonsoshi@gmail.com>
2024-03-08 21:58:45 +0100

    Add CREATE User functionality to controller.

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,8 +1,10 @@
 package com.capgemini.wsb.fitnesstracker.user.internal;
 
+import com.capgemini.wsb.fitnesstracker.user.api.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -23,9 +25,15 @@ class UserController {
     }
 
 
-    @PostMapping("/{id}")
-    public void addUserToDatabase(@PathVariable("id") Long id) {
+    @PostMapping("/{firstname}/{lastname}/{birthdate}/{email}")
+    public void addUserToDatabase(@PathVariable("firstname") String firstname,
+                                  @PathVariable("lastname") String lastname,
+                                  @PathVariable("birthdate") LocalDate birthdate,
+                                  @PathVariable("email") String email) {
 
+        User user = new User(firstname, lastname, birthdate, email);
+
+        userService.createUser(user);
     }
 
 }
