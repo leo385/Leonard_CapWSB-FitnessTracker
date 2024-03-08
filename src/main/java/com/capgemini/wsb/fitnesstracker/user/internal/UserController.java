8b249b90ca007dba3fc4970ac5b03abbc28a5c32
leonard <leonsoshi@gmail.com>
2024-03-08 15:23:18 +0100

    Configuration database target file

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -1,9 +1,7 @@
 package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,12 @@ class UserController {
                           .stream()
                           .map(userMapper::toDto)
                           .toList();
+    }
+
+
+    @PostMapping("/{id}")
+    public void addUserToDatabase(@PathVariable("id") Long id) {
+
     }
 
 }
